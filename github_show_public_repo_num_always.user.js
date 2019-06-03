9ff501ae7246605fa0c37ab8c8ae763b7e8c8750
oscardoudou <oscar9591@icloud.com>
2019-06-03 18:57:13 +0800

    use meaning static js path select element

--- github_show_public_repo_num_always.user.js
+++ github_show_public_repo_num_always.user.js
@@ -2,7 +2,7 @@
 // @name         github show public repo num when log in
 // @namespace    https://github.com/oscardoudou/
 // @version      0.1
-// @description  after having your github logged in, you probably just want to see public repos count instead of total amount including too many trivial private repos. If u have obsession, this is the script.
+// @description  after having your github logged in, you probably just want to see public repos count instead of total amount including too many trivial private repos. If u have obsession with you repo count, this is the script for u.
 // @author       oscardoudou
 // @match        *://github.com/*
 // @exclude      *://github.com/*/*
@@ -14,20 +14,11 @@
 (function() {
     'use strict'
     var user_login = $("meta[name='user-login']")[0].getAttribute('content')
-    //$("title")[0].innerText
-    var user_viewed
-    //extract username if has user being viewd has alias name
-    //var title = document.title
-    /* title doesn't always be username(alias) on tab other than overview
-    if(title.indexOf("(") != -1){
-        user_viewed = title.slice(0,title.indexOf("(")-1)
-    }*/
-    user_viewed = document.querySelector("#js-pjax-container > div > div.h-card.col-3.float-left.pr-6 > div.vcard-names-container.py-3.js-sticky.js-user-profile-sticky-fields > h1 > span.p-nickname.vcard-username.d-block").innerText
+    //var user_viewed = document.querySelector("#js-pjax-container > div > div.h-card.col-3.float-left.pr-6 > div.vcard-names-container.py-3.js-sticky.js-user-profile-sticky-fields > h1 > span.p-nickname.vcard-username.d-block").innerText
+    var user_viewed = document.getElementsByClassName('vcard-names')[0].lastElementChild.innerText
     console.log(user_viewed)
     //check if user being viewed is logged-in user
     if(user_viewed === user_login){
-        //below query works fine when using other's required script like ipcjs, the difference with current working one is the outer #js-pjax-container > div
-        //document.querySelector("#js-pjax-container > div > div.col-9.float-left.pl-2 > div.UnderlineNav.user-profile-nav.js-sticky.top-0.is-stuck > nav > a.UnderlineNav-item.selected > span")
         var request = new XMLHttpRequest()
         //use callback
         request.onload = changeRepoToPubRepoCount;
@@ -39,7 +30,7 @@
 })();
 
 function changeRepoToPubRepoCount(){
-    //use keyword this, otherwise using requset.responseText would get error when parsing
     var responseObj = JSON.parse(this.responseText)
-    document.querySelector("div.col-9.float-left.pl-2 > div.UnderlineNav.user-profile-nav.js-sticky.top-0 > nav > a:nth-child(2) > span").innerText=responseObj.public_repos;
+    //document.querySelector("div.col-9.float-left.pl-2 > div.UnderlineNav.user-profile-nav.js-sticky.top-0 > nav > a:nth-child(2) > span").innerText=responseObj.public_repos;
+    document.getElementsByClassName('Counter hide-lg hide-md hide-sm')[0].innerText = responseObj.public_repos
 }
