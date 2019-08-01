80b242bdf3e8820473f202bc1927f975ee468fa0
Niagara. O <oscar9591@icloud.com>
2019-08-01 14:02:34 -0400

    clean comment and apply on greasyfork

--- github_show_public_repo_num_always.user.js
+++ github_show_public_repo_num_always.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         github show public repo num when log in
 // @namespace    https://github.com/oscardoudou/
-// @version      0.1
-// @description  after having your github logged in, you probably just want to see public repos count instead of total amount including too many trivial private repos. If u have obsession with you repo count, this is the script for u.
+// @version      0.1.1
+// @description  after having your github logged in, you probably just want to see public repos count instead of total amount including too many trivial private repos. If u have obsession with ur repo count, this is the script for u.
 // @author       oscardoudou
 // @match        *://github.com/*
 // @exclude      *://github.com/*/*
@@ -14,9 +14,7 @@
 (function() {
     'use strict'
     var user_login = $("meta[name='user-login']")[0].getAttribute('content')
-    //var user_viewed = document.querySelector("#js-pjax-container > div > div.h-card.col-3.float-left.pr-6 > div.vcard-names-container.py-3.js-sticky.js-user-profile-sticky-fields > h1 > span.p-nickname.vcard-username.d-block").innerText
     var user_viewed = document.getElementsByClassName('vcard-names')[0].lastElementChild.innerText
-    console.log(user_viewed)
     //check if user being viewed is logged-in user
     if(user_viewed === user_login){
         var request = new XMLHttpRequest()
@@ -31,6 +29,5 @@
 
 function changeRepoToPubRepoCount(){
     var responseObj = JSON.parse(this.responseText)
-    //document.querySelector("div.col-9.float-left.pl-2 > div.UnderlineNav.user-profile-nav.js-sticky.top-0 > nav > a:nth-child(2) > span").innerText=responseObj.public_repos;
-    document.getElementsByClassName('Counter hide-lg hide-md hide-sm')[0].innerText = responseObj.public_repos
+    document.querySelector("div.UnderlineNav.user-profile-nav.js-sticky.top-0 > nav > a:nth-child(2) > span").innerText=responseObj.public_repos;
 }
