98e4ae32f22e9912aea6937316d3bbe9fe10242e
oscardoudou <oscar9591@icloud.com>
2019-09-19 22:21:47 -0400

    support blocking post with keywords & apply script on hupu main site

--- hupu_block.js
+++ hupu_block.js
@@ -1,25 +1,21 @@
 // ==UserScript==
 // @name         hupu block
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  block annoying people
 // @author       oscardoudou
-// @match        https://bbs.hupu.com/*
-// @grant        none
+// @match        *://*.hupu.com/*
+// @grant        Izzy
 // ==/UserScript==
 
 (function() {
     'use strict';
     //隐藏屏蔽的人的回帖
-    //no need to chain the class, continue use the dom element to find
-    //var $users = $('.floor .floor-show .floor_box .author .left .u')
-    var blocklist = ['user1','user2']
+    const blacklist = ['user1','user2','user3']
+    const keywords = ['keyword1', 'keyword2']
     var blocked
     $('.floor').each(function(index,floor){
-        //console.log($users[i])
-        //user[i] doesn't match index of $floor[i]
-        for (blocked of blocklist){
-            //use $ wrap the element you already got in a jQuery obj and continue select deeper element related to that dom element 
+        for (blocked of blacklist){
             if($(floor).find('.u')[0].innerText === blocked){
                 // console.log(floor)
                 floor.remove()
@@ -28,20 +24,28 @@
             }
         }
     })
+    //隐藏含关键字的帖子
+    $('.truetit').each(function(index,post){
+        for (blocked of keywords){
+           if(post.innerText.indexOf(blocked)!=-1){
+               console.log(post)
+               post.parentNode.parentNode.remove();
+               break;
+           }
+        }
+    })
     //console.log(document.readyState)
 })();
 
-//notification need wait for window ready, it loads after floor, as state shown as interactive in prev block
+//notification need wait for window ready. it loads after floor, as state shown as interactive in prev block
 $(window).load(function(){
-    var blocklist = ['user1','user2']
-    //从通知栏移除屏蔽人的回复，杜绝污染
-    console.log(document.readyState)
-    //forEach only work for array object, commonNotification is list
+    var blacklist = ['user1','user2','user3']
+    //从通知栏移除屏蔽人的回复
+    //console.log(document.readyState)
     var removed = 0
     var blocked
     $('.commonNotificationsInfo').each(function(index,notification){
-        // console.log(notification)
-        for ( blocked of blocklist){
+        for ( blocked of blacklist){
             if($(notification).find('a')[0].innerText === blocked){
                 notification.parentNode.removeChild(notification)
                 removed+=1
@@ -51,4 +55,3 @@ $(window).load(function(){
     //修改消息栏小红点未读消息数目
     $('.tip-topBubble.tip-topBubble-1')[0].innerText -= removed
 })
-
