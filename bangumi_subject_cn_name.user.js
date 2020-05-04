0ab200338a0e750c12ac71a7c2fc059ddeede05e
oscardoudou <oscar9591@icloud.com>
2020-05-04 01:09:19 -0400

    show cn title only available

--- bangumi_subject_cn_name.user.js
+++ bangumi_subject_cn_name.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         番组计划主页中文标题
 // @namespace    https://github.com/oscardoudou/
-// @version      0.1.2
+// @version      0.1.2.1
 // @description  Bangumi番组计划中文标题
 // @author       oscardoudou
 // @include      *://bgm.tv/
@@ -14,18 +14,21 @@
 (function() {
     'use strict'
     const texts = document.querySelectorAll('.infoWrapper_tv .headerInner .l.textTip')
-    texts.forEach((item) => {
-        item.innerHTML = item.getAttribute('data-subject-name-cn')
-    })
     const liNodeList = document.getElementById('prgSubjectList').querySelectorAll(".clearit")
-    liNodeList.forEach((item) => {
+    liNodeList.forEach((item,index) => {
         //anime
         if(item.getAttribute('subject_type') === "2"){
             var title_ch = item.children[3].getAttribute('data-subject-name-cn')
+            //only replace original title when cn title availablt
+           if(title_ch != ""){
             item.querySelectorAll("span")[1].innerHTML = title_ch
+            texts[index].innerHTML = texts[index].getAttribute('data-subject-name-cn')
+           }
         }
         //book
         if(item.getAttribute('subject_type') === "1"){
+            //todo book w/o cn title check
+            //detailview title replace
             title_ch = item.children[2].getAttribute('data-subject-name-cn')
             item.querySelectorAll("span")[1].innerHTML = title_ch
         }
