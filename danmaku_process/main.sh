05bf197be41d59f78808eda518d8ec61deee514b
oscardoudou <oscar9591@icloud.com>
2021-01-02 18:19:05 -0500

    hardcode some font related parameter hardcode sub delay add missing Events section column header

--- danmaku_process/main.sh
+++ danmaku_process/main.sh
@@ -4,22 +4,24 @@
 xmls=(); for i in {1..12}_*; do if [ -s "$i" ] ; then xmls[${i%%_*}]+=$i\ ; fi ; done
 # for i in ${!xmls[@]}; do echo ${xmls[i]}; done
 for i in ${!xmls[@]}; do 
-    danmaku2ass -o $i.ass -s 1920x1080 -fn "MS PGothic" -fs 48 -a 0.8 -dm 7 -ds 5 ${xmls[i]} ;
+    danmaku2ass -o $i.ass -s 1920x1080 -fs 25 -a 0.7 -dm 10 -ds 7 -p 100 ${xmls[i]} ;
     # need shift (add negative delay value to fastword the subtitle so it could sync)
-    ffmpeg -itsoffset -16 -i $i.ass -c copy danmaku_$(printf %02d.ass ${i%.ass}) && rm $i.ass ;
+    ffmpeg -itsoffset -13 -i $i.ass -c copy danmaku_$(printf %02d.ass ${i%.ass}) && rm $i.ass ;
 done
 
-# unify the encoding of subtitle ass
-for f in *Freezing*; do iconv -f utf-16 -t utf-8 < "$f" > sub_$(echo "$f" | sed -E 's/^.*Freezing\]\[([0-9]*)\]\[BD.*$/\1/g').ass ; done
+# unify the encoding of subtitle ass utilizing the cycle to rename episode as zero leading
+for f in *Freezing*.ass; do iconv -f utf-16 -t utf-8 < "$f" > sub_$(echo "$f" | sed -E 's/^.*Freezing\]\[([0-9]*)\]\[BD.*$/\1/g').ass ; done
 
 # merge begin
 for sub in sub_*.ass; do
     index=$(echo $sub | sed -E 's/^sub_([0-9]*).ass$/\1/g')
     danmaku=danmaku_$index.ass
+    # add the input validation check here for now
     if [ -f $danmaku ]; then
         ./merge_ass.sh $danmaku $sub
+        rm $danmaku $sub
     else
-        cp $sub ${sub%%.*}_danmaku.ass 
+        mv $sub ${sub%%.*}_danmaku.ass 
     fi
 done
 
--- danmaku_process/merge_ass.sh
+++ danmaku_process/merge_ass.sh
@@ -5,11 +5,14 @@ subtitle=$2
 #make sure both are utf-8 encoded, most likely ass would violate this rule
 merged=${subtitle%%.*}_danmaku.ass
 #currently primarily use danmaku's script info
-cat $danmaku | sed -n '/\Script/,/^[[:space:]]$/p' > $merged
+cat $danmaku | sed -n '/\[Script/,/^[[:space:]]$/p' > $merged
 #extract style section from danmaku, but exclude the empty line after 
 cat $danmaku | sed -n '/\[V/,/^[[:space:]]$/{/^[[:space:]]$/!p;}' >> $merged
 #append sustitle file's style at the end of danmaku's style 
-cat $subtitle | sed -n '/\Script/,/\[Events/p' | grep ^Style >> $merged
+cat $subtitle | sed -n '/\[Script/,/\[Events/p' | grep Style\: | awk 'BEGIN{FS=OFS=","}{$3=($3+4)*1.5; print $0}' >> $merged
+#append event section column header
+echo -en "\n" >> $merged
+cat $danmaku | sed -n '/\[Events/,/Format\:/p' >> $merged
 #concat extracted dialogue portions and sort based on start time of event
 cat <(sed -n '/\Dialogue/,$p' $danmaku) <(sed -n '/\Dialogue/,$p' $subtitle) | sort -t , -k 2 >> $merged
 
