5fbb295a61180afc030d37cc5755efa9aeee52a0
oscardoudou <oscar9591@icloud.com>
2021-01-04 00:32:36 -0500

    1.deal with separated danmaku files test when single episode have 2 parts 2.somehow reuse merge_ass 3.fix extract style section from danmaku resulting including dialogue, which causing style missing 4.add ugly option 5.hardcode increase sub file font step (eval fail bc can't figure out quote)

--- danmaku_process/main.sh
+++ danmaku_process/main.sh
@@ -2,11 +2,52 @@
 
 #convert xml to ass using Danmaku2Ass
 xmls=(); for i in {1..12}_*; do if [ -s "$i" ] ; then xmls[${i%%_*}]+=$i\ ; fi ; done
-# for i in ${!xmls[@]}; do echo ${xmls[i]}; done
 for i in ${!xmls[@]}; do 
-    danmaku2ass -o $i.ass -s 1920x1080 -fs 50 -a 0.7 -dm 10 -ds 7 -p 100 ${xmls[i]} ;
-    # need shift (add negative delay value to fastword the subtitle so it could sync)
-    ffmpeg -itsoffset -13 -i $i.ass -c copy danmaku_$(printf %02d.ass ${i%.ass}) && rm $i.ass ;
+    # read each part's danmaku file name into array, no need to unset IFS for env cmd
+    IFS=' ' read -ra arr <<< "${xmls[i]}"
+    ## extract cid from each part
+    #for p in ${arr[@]}; do
+    #    p=$(echo $p | sed -E 's/^[0-9]*_([0-9]*).*$/\1/g')
+    #    echo $p
+    #done
+    ## sort
+    #IFS=$'\n' parts=($(sort -n <<< "${arr[*]}"))
+    #unset IFS
+    #initially thought using *$p* to match the part 
+    
+    #use version sort
+    IFS=$'\n' parts=($(sort -V <<< "${arr[*]}"))
+    unset IFS
+    for j in ${!parts[@]}; do
+        temp_ass=${parts[j]%_*}.ass
+        danmaku2ass -o $temp_ass -s 1920x1080 -fs 50 -a 0.7 -dm 10 -ds 7 -p 100 ${parts[j]}
+        shift_ass=${i}_part_$(printf %02d.ass ${j%.ass}) 
+        if [ $j -gt 0 ];  then
+            if [ -n "$offset" ]; then
+                echo "${parts[j]} need shift $offset (positive delay)"
+                ffmpeg -v quiet -itsoffset $offset -i $temp_ass -c copy $shift_ass
+            fi
+        else
+            ffmpeg -v quiet -itsoffset -13 -i $temp_ass -c copy $shift_ass
+        fi
+        rm $temp_ass
+
+        # extract prev part last comments appear time as offset
+        offset=$(cat $shift_ass | grep "Dialogue\:" | tail -1 | awk -F "," '{ print $2 }')
+    done 
+done
+
+#put parts from same episode into same element of array
+danmakus=();
+for f in {1..12}_*.ass; do     
+    if [ -s $f ]; then
+        danmakus[${f%%_*}]+=$f\ ;
+    fi
+done
+
+#merge parts
+for i in ${!danmakus[@]}; do
+   ./merge_ass.sh -p ${danmakus[i]} 
 done
 
 # unify the encoding of subtitle ass utilizing the cycle to rename episode as zero leading
@@ -15,10 +56,10 @@ for f in *Freezing*.ass; do iconv -f utf-16 -t utf-8 < "$f" > sub_$(echo "$f" | 
 # merge begin
 for sub in sub_*.ass; do
     index=$(echo $sub | sed -E 's/^sub_([0-9]*).ass$/\1/g')
-    danmaku=danmaku_$index.ass
+    danmaku=$index.ass
     # add the input validation check here for now
     if [ -f $danmaku ]; then
-        ./merge_ass.sh $danmaku $sub
+        ./merge_ass.sh -s $danmaku $sub
         rm $danmaku $sub
     else
         mv $sub ${sub%%.*}_danmaku.ass 
--- danmaku_process/merge_ass.sh
+++ danmaku_process/merge_ass.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
-#usage $0 danmaku.ass subtitle.ass
-danmaku=$1
-subtitle=$2
-#make sure both are utf-8 encoded, most likely ass would violate this rule
-merged=${subtitle%%.*}_danmaku.ass
-#currently primarily use danmaku's script info
-cat $danmaku | sed -n '/\[Script/,/^[[:space:]]$/p' > $merged
-#extract style section from danmaku, but exclude the empty line after 
-cat $danmaku | sed -n '/\[V/,/^[[:space:]]$/{/^[[:space:]]$/!p;}' >> $merged
-#append sustitle file's style at the end of danmaku's style (probably could just grep directly)
-cat $subtitle | sed -n '/\[Script/,/\[Events/p' | grep Style\: | awk 'BEGIN{FS=OFS=","}{$3=($3+4)*2; print $0}' >> $merged
+usage="Usage: $0 [-p] [-s] ass [ass ...]"
+peer_mode=false
+scale=false
+# colon after option means require ARGUMENT
+while getopts ":ps" opt; do
+    case $opt in 
+        p) 
+            peer_mode=true;;
+        s)
+            scale=true;;
+        \?)
+            echo $usage
+            exit 1;;
+    esac
+done
+
+shift $((OPTIND-1))
+if [ -z "$*" ]; then
+    echo "$usage"
+fi
+
+base=$1
+addon=$2
+
+if [ $peer_mode = "false" ]; then
+    # ugh, just keep the final name as this for now, this wont incur changin burn.sh
+    merged=${addon%%.*}_danmaku.ass
+else
+    merged=$(echo $base | sed -E 's/^(.*)_part.*/\1/g' | xargs printf %02d ).ass
+fi
+# currently this check only handle peer_node merge when this is only one file involved, mixed mode merge is checked outside
+if [ -z "$addon" ]; then 
+    mv $base $merged
+    exit 0
+fi
+
+#currently primarily use base's script info
+cat $base | sed -n '/\[Script/,/^[[:space:]]$/p' > $merged
+#extract style section from base, but exclude the empty line after , head negative not working on mac, hardcode for now
+cat $base | sed -n '/\[V/,/\[Events/p' | grep -E Style\|Format >> $merged
+#append sustitle/addon file's style at the end of base's style (probably could just grep directly)
+if [ $scale = "false" ]; then
+    cat $addon | sed -n '/\[V/,/\[Events/p' | grep Style\: >> $merged
+else
+    cat $addon | sed -n '/\[V/,/\[Events/p' | grep Style\: | awk 'BEGIN{FS=OFS=","}{$3=($3+4)*2; print $0}' >> $merged
+fi
 #append event section column header
 echo -en "\n" >> $merged
-cat $danmaku | sed -n '/\[Events/,/Format\:/p' >> $merged
-#concat extracted dialogue portions and sort based on start time of event
-cat <(sed -n '/\Dialogue/,$p' $danmaku) <(sed -n '/\Dialogue/,$p' $subtitle) | sort -t , -k 2 >> $merged
+cat $base | sed -n '/\[Events/,/Format\:/p' >> $merged
 
+#concat extracted dialogue portions and sort based on start time of event
+cat <(sed -n '/\Dialogue/,$p' $base) <(sed -n '/\Dialogue/,$p' $addon) | sort -t , -k 2 >> $merged
+
+if [ $peer_mode = "true" ]; then
+    rm $base $addon
+fi
