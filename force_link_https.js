250f0c7fb936ca45d0bfcc265be2706ef6fc3fae
oscardoudou <oscar9591@icloud.com>
2021-02-27 19:41:05 -0500

    1. change https in matching rule as an option 2. unify the link host as bangumi.tv to avoid repeat login

--- force_link_https.js
+++ force_link_https.js
@@ -4,28 +4,32 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @include      /^https://(bangumi|bgm).tv.*$/
+// @include      /^https?://(bangumi|bgm).tv.*$/
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  'use strict';
 /*if(window.location.protocol != 'https:') {
-  location.href =   location.href.replace("http://", "https://");
+location.href =   location.href.replace("http://", "https://");
 }*/
 /*    if (location.protocol !== "https:") {
-  location.protocol = "https:";
+location.protocol = "https:";
 }*/
-      var current_host = window.location.host;
-  for (let a of document.querySelectorAll(`a.l`)){
-    if(["bangumi.tv", "bgm.tv"].includes(a.host)){
-      console.log(a)
-        console.log("current_host:" + current_host)
-        console.log("a.host:" + a.host)
-      if(a.protocol!="https:"){
-        a.protocol="https:"
-      }
+    var current_host = window.location.host;
+for (let a of document.querySelectorAll(`a.l`)){
+  if(["bangumi.tv", "bgm.tv"].includes(a.host)){
+    console.log(a)
+      console.log("current_host:" + current_host)
+      console.log("a.host:" + a.host)
+    //change a.host to bangumi.tv to avoid repeat login
+    if(a.host == "bgm.tv"){
+      a.host = "bangumi.tv"
+    }
+    if(a.protocol!="https:"){
+      a.protocol="https:"
     }
   }
-    // Your code here...
+}
+  // Your code here...
 })();
