0ecb92dc51534a10a5fc46a1f1572d226a3ca2e6
oscardoudou <oscar9591@icloud.com>
2021-03-29 23:57:35 -0400

    initial purpose of rename script doesn't seem to be work as the dir1 dir2 don't even have the dollar sign in front of them quickly make this script work, current usage ./rename.sh . . on local, its symlink is called renameSub under /usr/local/bin use this with addSub together

--- ltbs/pattern_pattern_matching/rename.sh
+++ ltbs/pattern_pattern_matching/rename.sh
@@ -1,25 +1,36 @@
 # 
 #   use name of mkv files in one directory  
 #   to rename ass files in another directory
-
+#   though it claims to be able to rename files using files in another file, but doesn't seem to be
+#   quickly make it work, currently only support ./rename . .
 dir1=$1
 dir2=$2
 #   if directory contains character need to escape
 #   most likely can't simply do ass=(dir1/*.mkv)
 #   hardcode dir1 if necessary 
 #   put all video files into an array
-mkvpaths=(dir1/*.mkv)
-#   strip leading path components
-mkvfiles=("${mkvpaths[@]##*/}")
-#   strip extension
-names=("${mkvfiles[@]%.*}")
+mkvpaths=($dir1/*.mkv)
+#echo $dir1
+#echo ${#mkvpaths[@]}
+#echo ${mkvpaths[10]##*/}
+##   strip leading path components
+#mkvfiles=("${mkvpaths[@]##*/}")
+#ehco ${mkvfiles[10]}
+#exit
+##   strip extension
+#names=("${mkvfiles[@]%.*}")
 #   put all asspath into an array
-asspaths=(dir2/*.ass)
-#   strip leading path components
-assfiles=("${asspaths[@]##*/}")
-#   loop through index in the list of array indexes
-for i in ${!assfiles[@]}
+asspaths=($dir2/*.ass)
+##   strip leading path components
+#assfiles=("${asspaths[@]##*/}")
+##   loop through index in the list of array indexes
+for i in ${!asspaths[@]}
 do
     #   rename the ass with name of mkv with same index from names array
-    mv "${assfiles[i]}" "${names[i]}".ass
+    name=${mkvpaths[i]##*/}
+    #echo name $name 
+    #echo ass ${asspaths[i]##*/}
+    #echo ${name%.*} 
+    #mv "${assfiles[i]##*/}" "${names[i]}".ass
+    mv ${asspaths[i]##*/}   ${name%.*}.ass
 done
