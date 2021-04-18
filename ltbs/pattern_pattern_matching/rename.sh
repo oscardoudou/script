39f70cd8e9c2383f013e01e64e261615b9b313c6
oscardoudou <oscar9591@icloud.com>
2021-04-17 22:16:14 -0400

    make renameSub dynamic

--- ltbs/pattern_pattern_matching/rename.sh
+++ ltbs/pattern_pattern_matching/rename.sh
@@ -1,36 +1,22 @@
-# 
-#   use name of mkv files in one directory  
-#   to rename ass files in another directory
-#   though it claims to be able to rename files using files in another file, but doesn't seem to be
-#   quickly make it work, currently only support ./rename . .
-dir1=$1
-dir2=$2
+#!/bin/bash
+#   use name of video files in one directory  
+#   to rename sub files in another directory
+#   though it claims to be able to rename files using files in another file, it doesn't seem to be as of last attempt
+#   right now to quickly make it work, currently only support ./rename videoExt subExt. .
+videoExt=$1
+subExt=$2
+dir1=${3:-.}
+dir2=${4:-.}
 #   if directory contains character need to escape
-#   most likely can't simply do ass=(dir1/*.mkv)
-#   hardcode dir1 if necessary 
+#   most likely can't simply do subpaths=(dir1/*.$videoExt)
 #   put all video files into an array
-mkvpaths=($dir1/*.mkv)
-#echo $dir1
-#echo ${#mkvpaths[@]}
-#echo ${mkvpaths[10]##*/}
-##   strip leading path components
-#mkvfiles=("${mkvpaths[@]##*/}")
-#ehco ${mkvfiles[10]}
-#exit
-##   strip extension
-#names=("${mkvfiles[@]%.*}")
-#   put all asspath into an array
-asspaths=($dir2/*.ass)
-##   strip leading path components
-#assfiles=("${asspaths[@]##*/}")
-##   loop through index in the list of array indexes
-for i in ${!asspaths[@]}
+videopaths=($dir1/*.$videoExt)
+#    put all subpath into an array
+subpaths=($dir2/*.$subExt)
+#   loop through index in the list of array indexes
+for i in ${!subpaths[@]}
 do
-    #   rename the ass with name of mkv with same index from names array
-    name=${mkvpaths[i]##*/}
-    #echo name $name 
-    #echo ass ${asspaths[i]##*/}
-    #echo ${name%.*} 
-    #mv "${assfiles[i]##*/}" "${names[i]}".ass
-    mv ${asspaths[i]##*/}   ${name%.*}.ass
+    #   rename the sub with name of video with same index from names array
+    name=${videopaths[i]##*/}
+    mv ${subpaths[i]##*/}   "${name%.*}.${subExt}"
 done
